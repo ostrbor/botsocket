bda9feb43cd7610a3645ca7eb3f96e087e80c45c
ostrbor <ostrbor@gmail.com>
2016-12-29 16:22:19 +0300

    refactored server;
    now its made of event loop, event handler

--- botsocket/server.py
+++ botsocket/server.py
@@ -3,10 +3,40 @@ import ssl
 import logging
 import socket
 from . import settings
-from .exceptions import BotSocketWrapperException
+from .exceptions import (BotSocketWrapperException,
+                         BotSocketBaseException)
 from .core import run_command
+from .utils import bin2dict, dict2bin, exc2bin
 
 logger = logging.getLogger(__name__)
+
+
+def _event_handler(connection):
+    try:
+        binary_request = connection.recv(recv)
+        request = bin2dict(binary_request)
+        response = run_command(binary_request)
+        logger.info('IP: %s, Request: %s, Response: %s' %
+                    (address[0], binary_request, response))
+    except BotSocketBaseException as e:
+        response = dict2bin(exc2bin(e))
+        logger.exception(response)
+    connection.sendall(response)
+
+
+def _event_loop(ssl_sock, recv):
+    while True:
+        try:
+            connection, address = ssl_sock.accept()  # address = (IP, PORT)
+        except ssl.SSLError as e:
+            logger.exception(str(e))
+            continue
+        if address[0] == settings.ALLOWED_HOST:
+            _event_handler(connection)
+        else:
+            msg = 'IP: %s is not allowed!' % address[0]
+            logger.warn(msg)
+        connection.close()
 
 
 def start_server(server_ip='0.0.0.0', recv=settings.BYTES_AMOUNT):
@@ -20,23 +50,4 @@ def start_server(server_ip='0.0.0.0', recv=settings.BYTES_AMOUNT):
         logger.exception(msg)
         raise BotSocketWrapperException(msg, e)
     ssl_sock.listen(settings.CONNECTIONS_IN_QUEUE)
-    while True:
-        try:
-            connection, address = ssl_sock.accept()  # address = (IP, PORT)
-        except ssl.SSLError as e:
-            logger.exception(str(e))
-            continue
-        if address[0] == settings.ALLOWED_HOST:
-            binary_request = connection.recv(recv)
-            try:
-                response = run_command(binary_request)
-                logger.info('IP: %s, Request: %s, Response: %s' %
-                            (address[0], binary_request, response))
-            except (CommandNotFoundError, BotSocketWrapperException) as e:
-                response = str(e)
-                logger.exception(response)
-            connection.sendall(dict2bin(response))
-        else:
-            msg = 'IP: %s is not allowed!' % address[0]
-            logger.warn(msg)
-        connection.close()
+    _event_loop(ssl_sock, recv)
