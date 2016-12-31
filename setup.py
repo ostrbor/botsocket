6c3ff531367d4c68722298064ce60141f5b5bb5c
ostrbor <ostrbor@gmail.com>
2016-12-31 13:25:24 +0300

    +readme.rst

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ def readme():
 
 setup(
     name='botsocket',
-    version='1.0',
+    version='0.1',
     description='socket server for bot communication',
     long_description=readme(),
     author='Boris Ostretsov',
     license='MIT',
     author_email='ostrbor@gmail.com',
-    keywords='bot botnet socket server simple',
+    keywords='bot botnet socket server',
     url='https://github.com/ostrbor/botsocket',
     packages=['botsocket'],
     install_requires=['cffi',
