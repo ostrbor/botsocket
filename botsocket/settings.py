ba446cca67ac307530ec7a224895e05ff0a68350
ostrbor <ostrbor@gmail.com>
2017-01-26 18:53:07 +0300

    Update settings with common constants for server and client

---
+++ botsocket/settings.py
@@ -0,0 +1,9 @@
+# Number of additional connections in queue to
+# hold and switch after current connection is processed.
+CONNECTIONS_IN_QUEUE = 0
+
+# Amount of bytes to fetch from socket
+BYTES_AMOUNT = 1024
+
+# Self-signed certificate.
+CERT_FILE = 'cert.pem'
