df24f844a96e1d7347156c717602da9caf723dc8
ostrbor <ostrbor@gmail.com>
2017-02-18 16:20:11 +0300

    Delete settings

--- botsocket/settings.py
+++
@@ -1,9 +0,0 @@
-# Number of additional connections in queue to
-# hold and switch after current connection is processed.
-CONNECTIONS_IN_QUEUE = 0
-
-# Amount of bytes to fetch from socket
-BYTES_AMOUNT = 1024
-
-# Self-signed certificate.
-CERT_FILE = 'cert.pem'
